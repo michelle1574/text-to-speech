a6d994a8adf9e7679b1a57bb70269b4efb285058
Michael <70703680+michelle1574@users.noreply.github.com>
2022-03-06 16:30:59 +0100

    Add files via upload

---
+++ TextToSpeech/Form1.vb
@@ -0,0 +1,22 @@
+﻿Public Class Form1
+    Dim SAPI
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        SAPI = CreateObject("SAPI.spvoice")
+        If CheckBox1.Checked = False Then
+            SAPI.Speak(TextBox1.Text)
+        Else
+            Do
+                SAPI.Speak(TextBox1.Text)
+            Loop
+        End If
+    End Sub
+
+
+    Private Sub CheckBox1_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles CheckBox1.CheckedChanged
+        If CheckBox1.Checked = True Then
+            Label2.Visible = True
+        Else
+            Label2.Visible = False
+        End If
+    End Sub
+End Class
